a02190d340004cbe65cbe9d406c0d4193567b302
NRS\nrsha <nrshakib007@gmail.com>
2023-09-30 14:52:15 +0600

    Added Routing following MVC Pattern

--- Server Side/index.js
+++ Server Side/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 const app = express();
 
@@ -12,9 +13,11 @@ dotenv.config();
 connectDB();
 
 // middlewares
-
 app.use(express.json());
 app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 //    REST APIs
 app.get("/", (req, res) => {
