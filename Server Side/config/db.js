3f4f3f6330e8906920d56186472b4a8cb54f1d58
NRS\nrsha <nrshakib007@gmail.com>
2023-09-30 15:12:58 +0600

    ConnectDB function modified

--- Server Side/config/db.js
+++ Server Side/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URL);
+    const dbConnect = await mongoose.connect(process.env.MONGO_URL);
     console.log("DB Connected");
   } catch (error) {
     console.log(error);
